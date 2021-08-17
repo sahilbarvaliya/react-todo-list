b5b850f59ed7501300e9e65d69f57394d5bc3ef4
Sahil <sahilbarvaliya2002@gmail.com>
2021-08-17 16:39:43 +0530

    hhh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react'
-import uuid from "uuid"
-import "bootstrap/dist/css/bootstrap.min.css"
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
-class App extends Component
-{
+import React, { Component } from "react";
+import uuid from "uuid/dist/v4";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+class App extends Component {
   state = {
-    items: [ { id: 1, title: "abcd" },
-    {id:2,title:"xyz"}]
-  }
+    items: [
+      { id: 1, title: "abcd" },
+      { id: 2, title: "xyz" },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("handleChange");
+  };
+  handleSubmit = (e) => {
+    console.log("handleSubmit");
+  };
+  clearList = () => {
+    console.log("clearlist");
+  };
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
+  };
   render() {
+    console.log(this.state);
     return (
-      <div>
-        <div className="container">
-          <div className="row">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
             <TodoInput />
             <TodoList />
           </div>
-       </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
